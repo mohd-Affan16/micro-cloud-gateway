18388bb24bdd2f1edf198f0d0a14734b31087950
Affan <affan.mohdaffan0216@gmail.com>
2026-08-07 16:31:14 +0530

    changes made for login

--- api/auth.js
+++ api/auth.js
@@ -1,9 +1,8 @@
 export default function handler(req, res) {
   const client_id = process.env.GOOGLE_CLIENT_ID; 
-  const redirect_uri = "https://micro-cloud-gateway.vercel.app"; // Your Vercel Domain Callback
+  const redirect_uri = "https://micro-cloud-gateway.vercel.app"; 
   
   const googleUrl = `https://google.com{client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=openid%20email`;
   
-  // Shift the browser seamlessly to Google's authentication cloud
   return res.redirect(googleUrl);
 }
