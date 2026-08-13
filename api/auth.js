52d5760afbe40e88fe0fb3799ba25b41b44af919
Affan <affan.mohdaffan0216@gmail.com>
2026-08-13 20:46:15 +0530

    change the link

--- api/auth.js
+++ api/auth.js
@@ -1,11 +1,11 @@
-export default function handler(req, res) {
-  // Hardcode the public tracking app ID directly to guarantee it reads the perfect key format
+module.exports = function handler(req, res) {
   const client_id = "://googleusercontent.com"; 
   const redirect_uri = "https://micro-cloud-gateway.vercel.app/api/callback"; 
   
-  const googleUrl = `https://google.com{client_id}&redirect_uri=${encodeURIComponent(redirect_uri)}&response_type=code&scope=openid%20email%20profile`;
+  const googleUrl = "https://google.com" + client_id + 
+                    "&redirect_uri=" + encodeURIComponent(redirect_uri) + 
+                    "&response_type=code&scope=openid%20email%20profile";
   
-  // Use native HTTP headers to force an un-crashable browser-level jump
   res.writeHead(302, { Location: googleUrl });
-  return res.end();
-}
+  res.end();
+};
